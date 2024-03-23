1742b571b0ff59f720df10e4361a8e2e413b6be0
Bayu K <50491719+bayukembara@users.noreply.github.com>
2024-03-23 08:17:49 +0700

    error handling

--- errorHandling/4.js
+++ errorHandling/4.js
@@ -1,8 +1,16 @@
 // Try Catch Error Final
 function div(a, b) {
+  if (b === 0) {
+    throw new Error("The Second Parameter can't be zero");
+  }
   return a / b;
 }
 try {
+  console.log(div(4, 0));
 } catch (e) {
+  console.log(e);
+  console.log(e.name);
+  console.log(e.message);
 } finally {
+  console.log("It's work");
 }
